90b2ac975399fe00ab2e3f5fbfd7544de70259c6
SUJP123 <sujaypatel04@gmail.com>
2025-01-13 15:30:42 -0500

    basic functionality

--- Github_CLI/app.js
+++ Github_CLI/app.js
@@ -1,4 +1,6 @@
-const http = require('node:http');
+import http from 'node:http';
+import readline from 'readline';
+import displayLogsForUser from './model.js';
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -9,6 +11,17 @@ const server = http.createServer((req, res) => {
     res.end('Hello, World!\n');
 })
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Welcome to the GITHUB CLI');
+console.log('---------------------------')
+
 server.listen(port, hostname, () => {
-    console.log(`Server running at host http://${hostname}:${port}/`)
+    rl.question('Input the username you want to check: ', (name) => {
+        console.log("\n")
+        displayLogsForUser(name);
+    });
 })
--- Github_CLI/model.js
+++ Github_CLI/model.js
@@ -1,9 +1,43 @@
 import axios from 'axios'
-import { Octokit } from "octokit";
 
 
-const octokit = new Octokit({ 
-    auth: process.env.API_KEY
-});
+async function displayLogsForUser(username) {
 
+    const resp = await axios.get(`https://api.github.com/users/${username}/events`).catch(
+        () => {
+            console.log('Username Not Found');
+    })
+    const data = resp.data;
+    console.log(data);
+    
+    const size = data.length;
+    
+    if (!size) {
+        console.log('No Recent Activity')
+        return;
+    }
 
+    let i = 0;
+    console.log("Output: \n");
+    for (i; i < size; i++) {
+        let log = data[i];
+        
+        if (log.type == "PushEvent") {
+            if (log.payload.size == 1) {
+                console.log(` - Pushed ${log.payload.size} commit to ${log.repo.name}`);
+            } else {
+            console.log(` - Pushed ${log.payload.size} commits to ${log.repo.name}`); 
+                }
+            }
+        else if (log.type == "PullRequestEvent") {
+            console.log(` - Made a Pull Request For ${log.repo.name}`);
+        } else if (log.type == "CreateEvent") {
+            console.log(` - Created Repo Called ${log.repo.name}`);
+        } else if (log.type == "DeleteEvent") {
+            console.log(` - Committed a deletion to ${log.repo.name}`);
+        }
+    }
+    return;
+}
+
+export default displayLogsForUser;
